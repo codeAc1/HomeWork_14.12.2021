daa87b8592dfd5b60eb0d19ee669f6de6a6af3c1
Sakhavat Aliyev <sakhavatra@code.edu.az>
2021-12-14 11:37:55 -0800

    Ready

---
+++ HomeWork_14.12.2021/ClcLib/Calculator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ClcLib
+{
+    public class Calculator
+    {
+        public void ClcMetod()
+        {
+            Console.WriteLine("\nUzerinde riyazi emeliyyatlar aparmaq ucun ededleri ve riyazi emeliyyatlarin acar karakterlerini daxil edin:");
+            double result = 0;
+            Console.Write("       1 ci ededi daxil edin: ");
+            double n = int.Parse(Console.ReadLine());
+
+            Console.Write("riyazi emeliyyati daxil edin: ");
+            char op = char.Parse(Console.ReadLine());
+
+            Console.Write("       2 ci ededi daxil edin: ");
+            double m = int.Parse(Console.ReadLine());
+
+
+
+            switch (op)
+
+            {
+
+                case '+':
+                    result = n + m;
+                    Console.WriteLine($"       Cavab: {n}+{m}=" + result); break;
+                case '-':
+                    result = n - m;
+                    Console.WriteLine($"       Cavab: {n}-{m}=" + result); break;
+                case '*':
+                    result = n * m;
+                    Console.WriteLine($"       Cavab: {n}*{m}=" + result); break;
+                case '/':
+                    result = n / m;
+                    Console.WriteLine($"       Cavab: {n}/{m}=" + result); break;
+
+                default:
+                    Console.WriteLine("       Duzgun emeliyyat daxil etmediniz\n");
+                    break;
+
+            }
+            Console.ReadLine();
+
+
+        }
+
+    }
+}
---
+++ HomeWork_14.12.2021/HomeWork_14.12.2021/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ClcLib;
+
+namespace HomeWork_14._12._2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Calculator calculator= new Calculator();
+            calculator.ClcMetod();        
+    }
+    }
+}
